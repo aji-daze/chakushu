5653b95f1d7aaa3a2e0e26b4fa05a3a625ffbd91
aji-daze <touka.arisugawa.0619@gmail.com>
2026-09-14 19:34:53 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "chakushu-v1";
+const CACHE = "chakushu-v2";
 const CORE = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png", "./apple-touch-icon.png"];
 
 self.addEventListener("install", e => {
